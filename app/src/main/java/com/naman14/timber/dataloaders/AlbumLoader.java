c31dedff0e13f3261cdd9d90af7e643d1160632d
WXYX_NB <1060279812@qq.com>
2016-07-14 13:06:02 +0800

    第二次提交

--- app/src/main/java/com/naman14/timber/dataloaders/AlbumLoader.java
+++ app/src/main/java/com/naman14/timber/dataloaders/AlbumLoader.java
@@ -30,6 +30,8 @@ public class AlbumLoader {
     public static Album getAlbum(Cursor cursor) {
         Album album = new Album();
         if (cursor != null) {
+
+
             if (cursor.moveToFirst())
                 album = new Album(cursor.getLong(0), cursor.getString(1), cursor.getString(2), cursor.getLong(3), cursor.getInt(4), cursor.getInt(5));
         }
